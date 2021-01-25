52656e8b4ea19c36aaec6138c22085e05618448b
AnkurLahiry <alahiry098@gmail.com>
2021-01-25 10:13:35 +0600

    Setup Project

---
+++ LinkedIn-Post/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LinkedIn-Post
+//
+//  Created by Ankur on 25/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
